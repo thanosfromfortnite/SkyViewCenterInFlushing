5fb62be6804f9c115b937ca693a2413eefdeed4c
BT_1N3_26 <jhan3537@bths.edu>
2018-11-26 11:34:11 -0500

    sdfsdfsd

--- src/SkyView.java
+++ src/SkyView.java
@@ -1,4 +1,10 @@
 public class SkyView {
+        /*
+      JESSE HAN
+      MR FOLWELL
+      PD 4/5 AP JAVA
+      DONUT STEEL
+       */
     private double[][] view;
 
     public SkyView(int numRows, int numCols, double[] scanned) {
@@ -31,3 +37,25 @@ public class SkyView {
         return (double) (total / numCounted);
     }
 }
+/*
+    We're talking away
+    I don't know what
+    I'm to say I'll say it anyway
+    Today's another day to find you
+    Shying away
+    I'll be coming for your love, okay?
+    Take on me (take on me)
+    Take me on (take on me)
+    I'll be gone
+    In a day or two
+    So needless to say
+    I'm odds and ends
+    But I'll be stumbling away
+    Slowly learning that life is okay
+    Say after me
+    It's no better to be safe than sorry
+    Take on me (take on me)
+    Take me on (take on me)
+    I'll be gone
+    In a day or two
+ */
--- src/SkyViewRunner.java
+++ src/SkyViewRunner.java
@@ -1,4 +1,10 @@
 public class SkyViewRunner {
+    /*
+    JESSE HAN
+    MR FOLWELL
+    PD 4/5 AP JAVA
+    DONUT STEEL
+     */
 
     public static void main(String[] args) {
         double[] scan = {0.3, 0.7, 0.8, 0.4, 1.4, 1.1, 0.2, 0.5, 0.2, 1.6, 0.6, 0.9};
@@ -14,3 +20,59 @@ public class SkyViewRunner {
         System.out.println("Average 2 " + sv2.getAverage(0, 1, 0, 1));
     }
 }
+/*
+    Like the legend of the phoenix
+    All ends with beginnings
+    What keeps the planet spinning (uh)
+    The force of love beginning
+    We've come too far to give up who we are
+    So let's raise the bar and our cups to the stars
+    She's up all night 'til the sun
+    I'm up all night to get some
+    She's up all night for good fun
+    I'm up all night to get lucky
+    We're up all night 'til the sun
+    We're up all night to get some
+    We're up all night for good fun
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    The present has no ribbon
+    Your gift keeps on giving,
+    What is this I'm feeling?
+    If you wanna leave I'm with it (ah)
+    We've come too far to give up who we are
+    So let's raise the bar and our cups to the stars
+    She's up all night 'til the sun
+    I'm up all night to get some
+    She's up all night for good fun
+    I'm up all night to get lucky
+    We're up all night 'til the sun
+    We're up all night to get some
+    We're up all night for good fun
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We've come too far to give up who we are
+    So let's raise the bar and our cups to the stars
+    She's up all night 'til the sun
+    I'm up all night to get some
+    She's up all night for good fun
+    I'm up all night to get lucky
+    We're up all night 'til the sun
+    We're up all night to get some
+    We're up all night for good fun
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+    We're up all night to get lucky
+ */
